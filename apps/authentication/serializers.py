5bf92dcdcf9b7003505087a68f047272e7890f10
kipsang <kipsang1998@gmail.com>
2025-05-31 18:37:11 +0300

    add customer serializer

--- apps/authentication/serializers.py
+++ apps/authentication/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from apps.customers.models import Customer
 
+
 class GoogleTokenSerializer(serializers.Serializer):
     """Serializer for Google ID token authentication"""
     id_token = serializers.CharField(required=True, help_text="Google ID token")
@@ -14,3 +15,25 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['id', 'email', 'first_name', 'last_name', 'date_joined']
         read_only_fields = ['id', 'date_joined']
+
+
+class CustomerSerializer(serializers.ModelSerializer):
+    """Serializer for Customer model"""
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Customer
+        fields = [
+            'id', 'user', 'phone', 'address', 'city',
+            'state', 'zip_code', 'date_of_birth',
+            'is_verified'
+        ]
+        read_only_fields = ['id', 'user']
+
+
+class CustomerUpdateSerializer(serializers.ModelSerializer):
+    """Serializer for updating customer profile"""
+
+    class Meta:
+        model = Customer
+        fields = ['phone', 'address', 'city', 'state', 'zip_code', 'date_of_birth']
