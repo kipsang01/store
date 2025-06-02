7ad81566714b12cebce419ce840980d811515e97
kipsang <kipsang1998@gmail.com>
2025-06-02 20:28:38 +0300

    add health check endpoint to verify server status

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from store.views import health_check
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('apps.authentication.urls')),
     path('api/customers/', include('apps.customers.urls')),
     path('api/orders/', include('apps.orders.urls')),
     path('api/', include('apps.products.urls')),
+    path('health-check/', health_check),
 ]
